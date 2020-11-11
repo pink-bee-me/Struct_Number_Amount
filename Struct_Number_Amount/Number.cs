dc12198ecd33eb1e0fc8bb0f20bfd895c46694b9
LaurieSue <pink.bee.me@outlook.com>
2020-11-11 01:04:13 -0600

    Add project files.

---
+++ Struct_Number_Amount/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Number_Amount
+{
+   public struct Number
+    {
+        public decimal Amount { get; set; }
+    
+    }
+}
---
+++ Struct_Number_Amount/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Number_Amount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amount1 = new Number();
+            amount1.Amount = .5678M;
+            Console.WriteLine("The Decimal Amount is " + amount1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
+
